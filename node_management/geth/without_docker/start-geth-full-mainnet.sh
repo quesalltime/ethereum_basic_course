60ae96479881b0bb17bc816ab6988644ecdffd07
quesalltime <losesong@gmail.com>
2019-07-06 19:35:55 +0800

    native without docker

--- node_management/geth/without_docker/start-geth-full-mainnet.sh
+++ node_management/geth/without_docker/start-geth-full-mainnet.sh
@@ -2,7 +2,7 @@
 
 geth_dir=$(pwd)
 geth \
---syncmode full --datadir "$geth_dir/gethdata" \
+--syncmode full --datadir "$geth_dir/gethdata/full_mainnet" \
 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 \
 --rpcapi "db,eth,net,web3,personal" \
 --rpcport 8545
